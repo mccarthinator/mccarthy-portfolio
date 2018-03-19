181cfd79a0d01503f749f7c5e8cfbaaf9d2b7dd5
mccarthinator <mccarthy.cath@gmail.com>
2018-03-19 11:48:11 -0500

    adding firebase setup

---
+++ js/movie-logic.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+  var config = {
+    apiKey: "<API_KEY>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+  };
+  firebase.initializeApp(config);
