f8f109d9f55c8684fdc5d6630043e704b051d508
mccarthinator <mccarthy.cath@gmail.com>
2018-03-20 09:55:12 -0500

    making it more simple

--- js/movie-logic.js
+++ js/movie-logic.js
@@ -1,14 +1,32 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyA3KyPdMZaiXV7XXIPAxMdbkWy0IVcilC0",
-    authDomain: "movie-scheduler.firebaseapp.com",
-    databaseURL: "https://movie-scheduler.firebaseio.com/",
-  };
+//   var config = {
+//     apiKey: "AIzaSyA3KyPdMZaiXV7XXIPAxMdbkWy0IVcilC0",
+//     authDomain: "movie-scheduler.firebaseapp.com",
+//     databaseURL: "https://movie-scheduler.firebaseio.com/",
+//   };
   
-  firebase.initializeApp(config);
+//   firebase.initializeApp(config);
 
-//   reference to firebase database
-  var movieData = firebase.database();
+// //   reference to firebase database
+//   var movieData = firebase.database();
+
+var movieArray = [
+    {name:"Liar Liar", length: 86, rating: "PG-13"},
+    {name:"There's Something About Mary", length: 134, rating: "R"},
+    {name:"Imitation Game", length: 114, rating: "PG-13"},
+    {name:"The Hateful Eight", length: 187, rating: "R"}
+];
+
+  // on button click, show each movie schedule results. 
+$("#movieButton").on("click", function(){
+    //display data of movie1
+})
+
+//on button click, clear these results.
+
+
+//define schedule rules
+
 
 
 
